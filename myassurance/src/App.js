404ff90a85ae09129e5957a9b33b6a725f75ce2f
Ciel Noir <oliverlavoie@hotmail.ca>
2025-04-02 13:51:50 -0400

    framework react

---
+++ myassurance/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import navbar from './components/navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    
+      <navbar />
+    </>
+  );
+}
+
+export default App;
